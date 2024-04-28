3782ca123411fbe2c2179bc22f80b231fb314b17
Ashish Tiwari <115717632+ashish-54321@users.noreply.github.com>
2024-04-29 00:49:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,36 @@
 const express = require('express');
 const axios = require('axios');
-const cheerio = require('cheerio'); // Library for parsing HTML
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static('public'));
 
 app.get('/search', async (req, res) => {
   try {
+    const query = req.query.q;
     const url = req.query.url;
 
-    if (!url) {
-      return res.status(400).send('Please provide a website URL');
+    if (!query && !url) {
+      return res.status(400).send('Please provide a search query or website URL');
     }
 
-    // Fetch the HTML content
-    const response = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
-      }
-    });
+    let response;
+    if (url) {
+      // If URL provided, fetch the content directly
+      const userAgent = req.headers['user-agent'];
+      response = await axios.get(url, {
+        headers: {
+          'User-Agent': userAgent
+        },
+      });
+    } else {
+      // If it's not a URL, search on Google
+      response = await axios.get(`https://www.google.com/search?q=${encodeURIComponent(query)}`);
+    }
 
-    // Load HTML content into Cheerio for easy manipulation
-    const $ = cheerio.load(response.data);
-
-    // Extract CSS and JavaScript file URLs from HTML
-    const cssUrls = [];
-    $('link[rel="stylesheet"]').each((index, element) => {
-      const cssUrl = $(element).attr('href');
-      cssUrls.push(cssUrl);
-    });
-
-    const jsUrls = [];
-    $('script[src]').each((index, element) => {
-      const jsUrl = $(element).attr('src');
-      jsUrls.push(jsUrl);
-    });
-
-    // Fetch CSS and JavaScript files asynchronously
-    const cssPromises = cssUrls.map(cssUrl => axios.get(cssUrl));
-    const jsPromises = jsUrls.map(jsUrl => axios.get(jsUrl));
-    const [cssResponses, jsResponses] = await Promise.all([Promise.all(cssPromises), Promise.all(jsPromises)]);
-
-    // Construct the response object with HTML, CSS, and JavaScript content
-    const responseData = {
-      html: response.data,
-      css: cssResponses.map(cssResponse => cssResponse.data),
-      js: jsResponses.map(jsResponse => jsResponse.data)
-    };
-
-    // Send the response
-    res.json(responseData);
+    // Forward the content to the client-side
+    res.send(response.data);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
